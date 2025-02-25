3fca267d8458f376758d86aac6ed7a2a10dc4abb
Abdullah <abdullahsoomro.official@gmail.com>
2025-02-25 20:31:27 +0500

    all zod schema will strip additional fields passed to it.

--- backend/src/utils/validate.util.ts
+++ backend/src/utils/validate.util.ts
@@ -4,6 +4,7 @@ import { ValidationError } from "./error.util";
 
 export const validate = (schema: ZodSchema) => {
   return (request: Request, _: Response, next: NextFunction): void => {
+    console.log("Request body: ", JSON.stringify(request.body, null, 2));
     try {
       if (!request.body) {
         throw new ValidationError("Request body is missing", null);
--- shared/interfaces.ts
+++ shared/interfaces.ts
@@ -28,10 +28,12 @@ export interface Order {
 /* DTO's */
 import { z } from "../backend/node_modules/zod";
 
-export const createProductDto = z.object({
-  name: z.string(),
-  price: z.number(),
-});
+export const createProductDto = z
+  .object({
+    name: z.string(),
+    price: z.number(),
+  })
+  .strip();
 
 export const updateProductDto = createProductDto
   .partial()
@@ -39,25 +41,29 @@ export const updateProductDto = createProductDto
     message: "At least one of 'name' or 'price' is required",
   });
 
-export const createStockDto = z.object({
-  productId: z.string(),
-  quantity: z.number().min(0),
-});
+export const createStockDto = z
+  .object({
+    productId: z.string(),
+    quantity: z.number().min(0),
+  })
+  .strip();
 
 export const updateStockDto = createStockDto.pick({ quantity: true });
 
-export const createOrderDto = z.object({
-  items: z
-    .object({
-      productId: z.string(),
-      quantity: z.number().min(0),
-    })
-    .array()
-    .refine((value) => value.length > 0, {
-      message: "Order must contain at least one item",
-    }),
-  discount: z.number().min(0).max(100),
-});
+export const createOrderDto = z
+  .object({
+    items: z
+      .object({
+        productId: z.string(),
+        quantity: z.number().min(0),
+      })
+      .array()
+      .refine((value) => value.length > 0, {
+        message: "Order must contain at least one item",
+      }),
+    discount: z.number().min(0).max(100),
+  })
+  .strip();
 
 export const updateOrderDto = createOrderDto;
 
