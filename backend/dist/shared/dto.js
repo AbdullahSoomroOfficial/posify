6238a7eaef891d4019940a051417084f9f933224
Abdullah <abdullahsoomro.official@gmail.com>
2025-03-16 04:02:57 +0500

    Refactor DTO exports and update zod import paths for consistency

--- backend/dist/shared/dto.js
+++ backend/dist/shared/dto.js
@@ -1,38 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateOrderDto = exports.createOrderDto = exports.updateStockDto = exports.createStockDto = exports.updateProductDto = exports.createProductDto = void 0;
+exports.updateOrderDto =
+  exports.createOrderDto =
+  exports.updateStockDto =
+  exports.createStockDto =
+  exports.updateProductDto =
+  exports.createProductDto =
+    void 0;
 /* DTO's */
-const zod_1 = require("../backend/node_modules/zod");
+const zod_1 = require("../../node_modules/zod");
 exports.createProductDto = zod_1.z
-    .object({
+  .object({
     name: zod_1.z.string(),
     price: zod_1.z.coerce.number(),
-})
-    .strip();
+  })
+  .strip();
 exports.updateProductDto = exports.createProductDto
-    .partial()
-    .refine((data) => Object.keys(data).length > 0, {
+  .partial()
+  .refine((data) => Object.keys(data).length > 0, {
     message: "At least one of 'name' or 'price' is required",
-});
+  });
 exports.createStockDto = zod_1.z
-    .object({
+  .object({
     productId: zod_1.z.string(),
     quantity: zod_1.z.coerce.number().min(0),
-})
-    .strip();
+  })
+  .strip();
 exports.updateStockDto = exports.createStockDto.pick({ quantity: true });
 exports.createOrderDto = zod_1.z
-    .object({
+  .object({
     items: zod_1.z
-        .object({
+      .object({
         productId: zod_1.z.string(),
         quantity: zod_1.z.number().min(1),
-    })
-        .array()
-        .refine((value) => value.length > 0, {
+      })
+      .array()
+      .refine((value) => value.length > 0, {
         message: "Order must contain at least one item",
-    }),
+      }),
     discount: zod_1.z.number().min(0).max(100),
-})
-    .strip();
+  })
+  .strip();
 exports.updateOrderDto = exports.createOrderDto;
